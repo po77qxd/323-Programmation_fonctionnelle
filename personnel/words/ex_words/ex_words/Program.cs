39532b80fdce08c01f2e1c18518eabcf2debcd37
Bastien <po77qxd@eduvaud.ch>
2025-09-03 10:34:19 +0200

    feat(ex words): partie 1B

--- personnel/words/ex_words/ex_words/Program.cs
+++ personnel/words/ex_words/ex_words/Program.cs
@@ -4,13 +4,21 @@
     {
         static void Main(string[] args)
         {
-            string[] words = { "bonjour", "hello", "monde", "vert", "rouge", "bleu", "jaune" };
+            //1A
+            //string[] words = { "bonjour", "hello", "monde", "vert", "rouge", "bleu", "jaune" };
 
-            words.Where(x => !x.Contains("x")).Reverse().ToList().ForEach(x => Console.WriteLine($"ne contiennent pas la lettre x: {x}"));
+            //words.Where(x => !x.Contains("x")).Reverse().ToList().ForEach(x => Console.WriteLine($"ne contiennent pas la lettre x: {x}"));
 
-            words.Where(x => x.Length >= 4).OrderBy(x => x).ToList().ForEach(x => Console.WriteLine($":ont 4 charactères ou plus: {x}"));
+            //words.Where(x => x.Length >= 4).OrderBy(x => x).ToList().ForEach(x => Console.WriteLine($":ont 4 charactères ou plus: {x}"));
 
-            words.Where(x => x.Length > words.Average(x => x.Length)).OrderByDescending(x => x).ToList().ForEach(x => Console.WriteLine($":ont autant de caractères que la moyenne du nombre de caractères de la liste: {x}"));
+            //words.Where(x => x.Length > words.Average(x => x.Length)).OrderByDescending(x => x).ToList().ForEach(x => Console.WriteLine($":ont autant de caractères que la moyenne du nombre de caractères de la liste: {x}"));
+
+            //1B
+            string[] words = { "whatThe!!!", "bonjour", "hello", "monde", "vert", "rouge", "bleu", "jaune", "My kingdom for a horse !", "Ooops I did it again" };
+
+            words.Where(x => x != words.ElementAt(0) && x != words.ElementAt(words.Length - 1) && x != words.ElementAt(words.Length - 2))
+                .ToList().ForEach(x => Console.WriteLine($"Données parasite 1: {x}"));
+
         }
     }
 }
