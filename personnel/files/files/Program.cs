058b4358195a9752c8e7fc9b3e495978bc3aacef
Bastien <po77qxd@eduvaud.ch>
2025-10-29 10:43:17 +0100

    feat(files): étape 2

--- personnel/files/files/Program.cs
+++ personnel/files/files/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace files
+﻿using System.IO;
+
+namespace files
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string path = "../../../Projet";
+            ListerFichiers(path);
+        }
+        static void ListerFichiers(string chemin)
+        {
+            if (!Directory.Exists(chemin))
+            {
+                return;
+            }
+            Directory.GetFiles(chemin).ToList().ForEach(file => Console.WriteLine(Path.GetFileName(file)));
+            Directory.GetDirectories(chemin).ToList().ForEach(directory =>
+            {
+                Console.WriteLine(Path.GetFileName(directory));
+                ListerFichiers(directory);
+            });
         }
     }
 }
